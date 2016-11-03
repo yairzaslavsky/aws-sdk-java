d68c5334a77e9703e0fcfaccc60375bab9a5fba0
Yair Zaslavsky <yzaslavsky@gmail.com>
2016-11-03 13:53:29 +1100

    issue #674

---
+++ aws-java-sdk-dynamodb/src/main/java/com/amazonaws/services/dynamodbv2/datamodeling/DynamoDBAnyGetter.java
@@ -0,0 +1,12 @@
+package com.amazonaws.services.dynamodbv2.datamodeling;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@DynamoDB
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface DynamoDBAnyGetter {
+}
